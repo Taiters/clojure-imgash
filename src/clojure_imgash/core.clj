c294741a43bcd6b7883fcb5f0d29fb59e63852f5
Daniel Tait <dantait91@gmail.com>
2016-09-30 23:35:36 +0100

    basic image generation

--- src/clojure_imgash/core.clj
+++ src/clojure_imgash/core.clj
@@ -3,9 +3,23 @@
   (use mikera.image.colours)
   (:gen-class))
 
+(defn abs [n]
+  (max n (- 0 n)))
+
+(defn between [n low high]
+  (and (>= low n) (< n high)))
+
+(defn get-rgb [h' c x]
+  (let [to-drop (* 3 (Math/floor h'))]
+    (take 3 (drop to-drop [c x 0 x c 0 0 c x 0 x c x 0 c c 0 x]))))
+
 (defn hsl-to-rgb [h s l]
-  (let [c (* (- 1 (Math/abs (- (* 2 l) 1))) s)] 
-    (println c)))
+  (let [c (* (- 1 (abs (- (* 2 l) 1))) s)
+        h' (/ h 60.0)
+        x (* c (- 1 (abs (- (mod h' 2) 1))))
+        rgb (get-rgb h' c x)
+        m (- l (/ c 2))]
+    (map #(+ % m) rgb)))
 
 (defn mirror-row [row blocks]
   (if (even? blocks)
@@ -27,16 +41,27 @@
         border (repeat border-pixel-count (first colours))]
     (flatten (concat border rows border))))
 
-(defn create-img [blocks block-size colours]
-  (let [size (* (+ blocks 2) block-size)
+(defn hue-to-colour [hue]
+  (apply rgb (hsl-to-rgb (mod hue 360) 0.6 0.5)))
+
+(defn get-pallete
+  ([n] (get-pallete n [(rand 360)]))
+  ([n hues]
+    (if (> n (count hues))
+      (recur n (cons (+ (first hues) (+ 100 (rand 160))) hues))
+      (map hue-to-colour hues))))
+
+(defn create-img [blocks block-size]
+  (let [colours (get-pallete 2)
+        size (* (+ blocks 2) block-size)
         img (new-image size size)
         pixels (get-img-pixels blocks block-size colours)]
-    (println (count pixels))
-    (println (count (get-pixels img)))
     (set-pixels img (int-array pixels))
     img))
 
 (defn -main
   [& args]
-
-  (show (time (create-img 7 20 (repeatedly 2 rand-colour)))))
+  ;(println (map #(* % 255) (hsl-to-rgb 0 0 0))))
+  ;(println (get-pallete 2)))
+  (dotimes [n 1000]
+    (show (create-img 7 30))))
