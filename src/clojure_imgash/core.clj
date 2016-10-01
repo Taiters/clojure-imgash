fd176aecfdd97807e00fcd58d272ac5fd0cf3ab6
Daniel Tait <dantait91@gmail.com>
2016-10-01 13:05:17 +0100

    generating based on hashed string

--- src/clojure_imgash/core.clj
+++ src/clojure_imgash/core.clj
@@ -6,16 +6,13 @@
 (defn abs [n]
   (max n (- 0 n)))
 
-(defn between [n low high]
-  (and (>= low n) (< n high)))
-
 (defn get-rgb [h' c x]
   (let [to-drop (* 3 (Math/floor h'))]
     (take 3 (drop to-drop [c x 0 x c 0 0 c x 0 x c x 0 c c 0 x]))))
 
 (defn hsl-to-rgb [h s l]
   (let [c (* (- 1 (abs (- (* 2 l) 1))) s)
-        h' (/ h 60.0)
+        h' (/ (mod h 360) 60.0)
         x (* c (- 1 (abs (- (mod h' 2) 1))))
         rgb (get-rgb h' c x)
         m (- l (/ c 2))]
@@ -26,42 +23,51 @@
     (concat row (reverse row))
     (concat row (rest (reverse row)))))
 
-(defn get-row-pixels [blocks block-size colours]
+(defn get-random [items r]
+  (nth items (.nextInt r (count items))))
+
+(defn get-row-pixels [blocks block-size colours r]
   (let [bg (first colours)
-        random-colour (partial rand-nth colours)
+        random-colour (partial get-random colours r)
         half-blocks (int (Math/ceil (/ blocks 2)))
         half-row (cons bg (repeatedly half-blocks random-colour))
         row (mirror-row half-row blocks)]
     (repeat block-size (map #(repeat block-size %) row))))
 
-(defn get-img-pixels [blocks block-size colours]
-  (let [row-func (partial get-row-pixels blocks block-size colours)
+(defn get-img-pixels [blocks block-size colours r]
+  (let [row-func (partial get-row-pixels blocks block-size colours r)
         rows (repeatedly blocks row-func)
         border-pixel-count (* (* block-size block-size) (+ blocks 2))
         border (repeat border-pixel-count (first colours))]
     (flatten (concat border rows border))))
 
-(defn hue-to-colour [hue]
-  (apply rgb (hsl-to-rgb (mod hue 360) 0.6 0.5)))
+(defn generate-colours [r]
+  (let [bg (.nextInt r 360)
+        fg (+ bg (+ 120 (.nextInt r 120)))]
+    (map #(apply rgb %) [(hsl-to-rgb bg 0.6 0.6)
+                         (hsl-to-rgb fg 0.75 0.6)])))
 
-(defn get-pallete
-  ([n] (get-pallete n [(rand 360)]))
-  ([n hues]
-    (if (> n (count hues))
-      (recur n (cons (+ (first hues) (+ 100 (rand 160))) hues))
-      (map hue-to-colour hues))))
-
-(defn create-img [blocks block-size]
-  (let [colours (get-pallete 2)
+(defn create-img [blocks block-size input]
+  (let [r (java.util.Random. (hash input))
+        colours (generate-colours r)
         size (* (+ blocks 2) block-size)
         img (new-image size size)
-        pixels (get-img-pixels blocks block-size colours)]
+        pixels (get-img-pixels blocks block-size colours r)]
     (set-pixels img (int-array pixels))
     img))
 
+(defn write-image [n]
+  (let [img (create-img 9 10 n)]
+    (save img (format "images/%s.png" (clojure.string/replace n #"\W+" "_")))))
+
 (defn -main
   [& args]
+  (let [stdin (line-seq (java.io.BufferedReader. *in*))]
+    (dorun
+      (pmap write-image stdin))
+    (shutdown-agents)))
+
+
   ;(println (map #(* % 255) (hsl-to-rgb 0 0 0))))
   ;(println (get-pallete 2)))
-  (dotimes [n 1000]
-    (show (create-img 7 30))))
+
